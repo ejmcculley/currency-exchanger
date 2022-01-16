3254b71478f06a4865333e79ac8d3278368df925
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-01-15 16:22:53 -0800

    Add fetch api and static asyn method to exchange.js, add ui logic to index.js, update HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './exchange.js';
+
+const clearFields = () => {
+  $(".showExchange").val("");
+  $(".showErrors").val("");
+};
+
+function getElements(response) {
+  if (response) {
+    $("#main").show();
+    $("#splash").hide();
+  } else {
+    $("showError").text(`The following error occurred: ${response.message}`);
+  }
+}
+
+async function makeApiCall() {
+  const response = await Exchange.getExchange();
+  getElements(response);
+}
+
+$(document).ready(() => {
+  $("#enter").click (() => {
+    clearFields();
+    makeApiCall();
+  });
+});
