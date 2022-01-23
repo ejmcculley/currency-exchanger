b3bb23950dd54438eb99a49681b7b13278454ad1
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-01-22 20:34:20 -0800

    Remove sensitive data from exchange.js history

---
+++ src/exchange.js
@@ -0,0 +1,13 @@
+export default class Exchange {
+  static async getExchange(currency1, currency2, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
